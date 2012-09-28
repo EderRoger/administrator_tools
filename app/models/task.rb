a45d505f6203dc63c7665a479cfe5f3d745d30c7
Eder Roger de Souza <eder.roger.souza@gmail.com>
2012-09-27 23:18:49 -0300

    configuration of  dashboard and tasks details

--- app/models/task.rb
+++ app/models/task.rb
@@ -4,4 +4,8 @@ class Task < ActiveRecord::Base
   belongs_to :admin_user
   validates :title, :project_id, :admin_user_id, :presence => true
   validates :is_done, :inclusion => { :in => [true, false] }
+
+  
+
+
 end
